a7c4a47a9485584a325cb14ed871798bab190225
rajdeep66 <rajdeep.jgd@gmail.com>
2017-09-22 16:37:46 +0530

    with unitest

---
+++ with testing/unittest/src/main/java/guru/springframework/unittest/quickstart/App.java
@@ -0,0 +1,7 @@
+public class App
+{
+    public String concatAndConvertString(String str1, String str2){
+        String concatedString=str1.concat(str2);
+        return concatedString.toUpperCase();
+    }
+}
---
+++ with testing/unittest/src/test/guru/springframework/unittest/quickstart/AppTest.java
@@ -0,0 +1,12 @@
+package guru.springframework.unittest.quickstart;
+
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase {
+    public void tearDown() throws Exception {
+    }
+
+    public void testConcatAndConvertString() throws Exception {
+    }
+
+}
